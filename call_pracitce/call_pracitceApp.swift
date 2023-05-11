148528b0432448741b92be361f207a8168fb94e5
이준후 <hprize04@gmail.com>
2023-05-11 10:06:34 +0900

    Initial Commit

---
+++ call_pracitce/call_pracitceApp.swift
@@ -0,0 +1,17 @@
+//
+//  call_pracitceApp.swift
+//  call_pracitce
+//
+//  Created by 이준후 on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct call_pracitceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
